a50ed83d3f06f8c4638556a55f56c24f9882ddde
Teodor Vidakovic <teodorv22@gmail.com>
2023-11-14 11:58:09 +0100

    feat: add simulated data in response body

--- app/api/endpoints/player_stats.py
+++ app/api/endpoints/player_stats.py
@@ -1,10 +1,38 @@
 # app/api/endpoints/player_stats.py
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+
+from app.api.core.models.PlayerStats import PlayerStats
 
 router = APIRouter()
 
 
-@router.get("/player/{playerFullName}")
-def get_player_info():
-    return {"message": "Player information endpoint"}
+@router.get("/player/{playerFullName}", response_model=PlayerStats)
+def get_player_info(playerFullName: str):
+    player_data = {
+        "playerName": playerFullName,
+        "gamesPlayed": 10,
+        "traditional": {
+            "freeThrows": {"attempts": 13.7, "made": 8.3, "shootingPercentage": 66.3},
+            "twoPoints": {"attempts": 20.5, "made": 12.8, "shootingPercentage": 62.4},
+            "threePoints": {"attempts": 7.2, "made": 3.5, "shootingPercentage": 48.6},
+            "points": 24.4,
+            "rebounds": 12.1,
+            "blocks": 4.3,
+            "assists": 9.7,
+            "steals": 3.1,
+            "turnovers": 5,
+        },
+        "advanced": {
+            "valorization": 50.6,
+            "effectiveFieldGoalPercentage": 55.2,
+            "trueShootingPercentage": 58.7,
+            "hollingerAssistRatio": 30.5,
+        }
+    }
+
+    # Check if the player is found
+    if playerFullName.lower() != "john doe":
+        raise HTTPException(status_code=404, detail="Player not found")
+
+    return PlayerStats(**player_data)
