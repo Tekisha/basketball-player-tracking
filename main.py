18085c80d332fb286e7976eb8fe4dc6386d4bfaf
Teodor Vidakovic <teodorv22@gmail.com>
2023-11-14 12:35:57 +0100

    feat: add logic for loading input data in main.py

--- main.py
+++ main.py
@@ -2,6 +2,20 @@
 
 from fastapi import FastAPI
 from app.api.routers import main as main_router
+from app.repositories.player_repository import create_in_memory_database, load_data_into_database
+
+csv_file_path = "data/input_file_1.csv"
+
+connection, cursor = create_in_memory_database()
+load_data_into_database(cursor, csv_file_path)
+
+cursor.execute('SELECT * FROM player_stats')
+result = cursor.fetchall()
+
+for row in result:
+    print(row)
+
+connection.close()
 
 app = FastAPI()
 
